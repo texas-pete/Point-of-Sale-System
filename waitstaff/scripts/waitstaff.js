aeec454d57099cf58e4c536061fafce94974923c
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-10-24 10:37:28 -0500

    Added a directory for waitstaff before our files

---
+++ waitstaff/scripts/waitstaff.js
@@ -0,0 +1,29 @@
+// applies functionality for table buttons to all table buttons
+$(document).ready(function () {
+    // boxButton onClick
+    $('.boxButton').click(function () {
+
+      // table number of button being pressed
+      var btnTblNum = $(this).first().text();
+      // table number on sidebar
+      var sidebarTblNum = $('#tblNumber').text();
+
+        if( $('#sidebar').hasClass('active') ){
+          // if table is already on screen, hide sidebar
+          if( btnTblNum == sidebarTblNum ){
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('boxButtonSelected');
+          }else{ //else, put new info in sidebar
+            $('.boxButtonSelected').toggleClass('boxButtonSelected');
+            $('#tblNumber').text(btnTblNum);
+            $(this).toggleClass('boxButtonSelected');
+          }      
+        }else{ //else, put new number and show
+          $('#tblNumber').text(btnTblNum);
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('boxButtonSelected');
+        }
+        
+    });
+
+});
