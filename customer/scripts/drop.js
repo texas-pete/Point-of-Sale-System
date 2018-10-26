35cd8ee3411fd40275646712b61fc9033626e1ed
VallejoAgustin <AgustinVallejo@my.unt.edu>
2018-10-26 03:18:00 -0500

    renamed dropdown code and stylesheet classes

--- customer/scripts/drop.js
+++ customer/scripts/drop.js
@@ -1,11 +1,11 @@
-function myFunction(){
-	document.getElementById("myDropdown").classList.toggle("show");
+function showDrinks(){
+	document.getElementById("ass-drop").classList.toggle("show");
 }
 
 window.onclick = function(event){
-	if (!event.target.matches('.dropbtn')) {
+	if (!event.target.matches('.ass-dropbtn')) {
 
-	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var dropdowns = document.getElementsByClassName("ass-dropdown-content");
 	var i;
 	for (i = 0; i < dropdowns.length; i++) {
 			var openDropdown = dropdowns[i];
