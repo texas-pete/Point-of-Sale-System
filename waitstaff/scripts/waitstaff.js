3e584e7607ea26a5b8763fc3fcba1ee7eca3bbf5
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-10-25 21:59:22 -0500

    Fixed Discord
    Zac helped me fix the sidebar

--- waitstaff/scripts/waitstaff.js
+++ waitstaff/scripts/waitstaff.js
@@ -10,20 +10,20 @@ $(document).ready(function () {
 
         if( $('#sidebar').hasClass('active') ){
           // if table is already on screen, hide sidebar
-          if( btnTblNum == sidebarTblNum ){
+          if( btnTblNum === sidebarTblNum ){
             $('#sidebar').toggleClass('active');
             $(this).toggleClass('boxButtonSelected');
           }else{ //else, put new info in sidebar
             $('.boxButtonSelected').toggleClass('boxButtonSelected');
             $('#tblNumber').text(btnTblNum);
             $(this).toggleClass('boxButtonSelected');
-          }      
+          }
         }else{ //else, put new number and show
           $('#tblNumber').text(btnTblNum);
           $('#sidebar').toggleClass('active');
           $(this).toggleClass('boxButtonSelected');
         }
-        
+
     });
 
 });
