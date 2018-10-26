eb93906d33353f78231a0bb622aeb7e3a40089d0
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-10-25 21:20:12 -0500

    Dak's
    Hope this doesn't break anything

---
+++ waitstaff/scripts/hideElement.js
@@ -0,0 +1,43 @@
+function hideContent(whichDiv) {
+  var x = document.getElementById("content"); //CONTENT is the div we wish to hide
+  var y = document.getElementById("payment");
+  var z = document.getElementById("service");
+
+  if(whichDiv === "content")
+  {
+    //we want to hide the payment and service code
+    y.style.display = "none";
+    z.style.display = "none";
+
+    //we want to show the content information
+    x.style.display = "block";
+  }
+  else if(whichDiv === "payment")
+  {
+    //we want to hide the content and service code
+    x.style.display = "none";
+    z.style.display = "none";
+
+    //we want to show the payment information
+    y.style.display = "block";
+  }
+  else if(whichDiv === "service")
+  {
+
+    //we want to hide the content and payment code
+    x.style.display = "none";
+    y.style.display = "none";
+
+    //we want to show the content information
+    z.style.display = "block";
+    // var x = document.getElementById("content"); //CONTENT is the div we wish to hide
+    // if (x.style.display === "none") {
+    //     x.style.display = "block";
+    // } else {
+    //     x.style.display = "none";
+    // }
+  }
+}
+
+
+// copy pasted from w3 schools
