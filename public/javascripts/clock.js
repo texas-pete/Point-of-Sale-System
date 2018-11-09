661876911d01eefec3f0172098cb3692ae367951
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-11-09 17:47:24 -0600

    Moving to desktop

---
+++ public/javascripts/clock.js
@@ -0,0 +1,27 @@
+function clock(){
+    s++;
+    if(s==60){
+        s=0;
+        m++;
+        if(m==60){
+            m=0;
+            h++;
+            if(h==24){
+                h=0;
+            }
+        }
+    }
+    $('sec',s);
+    $('min',m);
+    $('hr',h);
+    animate=setTimeout(clock,1000);
+};
+
+function $(id,val){
+    if(val<10){
+        val='0'+val;
+    }
+    document.getElementById(id).innerHTML=val;
+};
+
+window.onload=init;
