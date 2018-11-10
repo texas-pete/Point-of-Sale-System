ffeb5e3f4651a8ac0a14b38c56c9e5c60ceadc5e
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-11-09 18:21:38 -0600

    Clock Working
    (Needs a style sheet)

--- public/javascripts/clock.js
+++ public/javascripts/clock.js
@@ -1,27 +1,49 @@
-function clock(){
+var d, h, m, s, ampm, animate;
+
+function init() {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    clock();
+};
+
+function clock() {
     s++;
-    if(s==60){
-        s=0;
+    if (h > 12) {
+        ampm = h - 12;
+    }
+    if (s == 60) {
+        s = 0;
         m++;
-        if(m==60){
-            m=0;
-            h++;
-            if(h==24){
-                h=0;
+        if (m == 60) {
+            m = 0;
+            ampm++;
+            if (ampm > 12) {
+                ampm = 0;
             }
         }
     }
-    $('sec',s);
-    $('min',m);
-    $('hr',h);
-    animate=setTimeout(clock,1000);
+    $('ampm', s);
+    $('min', m);
+    $('hr', ampm);
+    animate = setTimeout(clock, 1000);
 };
 
-function $(id,val){
-    if(val<10){
-        val='0'+val;
+function $(id, val) {
+    if (id === 'ampm') {
+        if (h > 12) {
+            val = 'PM';
+        }
+        else {
+            val = 'AM';
+        }
+
     }
-    document.getElementById(id).innerHTML=val;
+    // if (val < 10) {
+    //     val = '0' + val;
+    // }
+    document.getElementById(id).innerHTML = val;
 };
 
-window.onload=init;
+window.onload = init;
