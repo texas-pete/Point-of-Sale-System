ca65028fa33802d2dad79fecffc574cc43348b91
zacferris <36178696+zacferris@users.noreply.github.com>
2018-11-10 19:34:29 -0600

    Basic socket working in customer view
    Service request now triggers a socket message.

--- public/javascripts/guestAssistanceHeader.js
+++ public/javascripts/guestAssistanceHeader.js
@@ -3,13 +3,23 @@
 var theParent = document.querySelector("#requester"); //this creates a listener for this class.
 theParent.addEventListener("click", validateSelection, false); //no idea what false does. don't really care either.
 
+var socket = io();
+
+socket.on('clearServiceRequest', function(id){
+    alert(id);
+});
+
+
 function validateSelection(e) //e is the entity which was pressed.
 {
     if (e.target !== e.currentTarget) {
         var interactedItem = e.target.id; //saves the ID of the element clicked
 
         if (interactedItem === "servicerq") {
+            // emit a service request event
+            socket.emit("serviceRequest", "message");
             alert("Hello " + interactedItem);
+            
         }
         else if (interactedItem === "drinkrq") {
             alert("Hello " + interactedItem);
