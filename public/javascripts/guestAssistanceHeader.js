9a74a2f6a3f038723d408030536a23b93915dac0
zacferris <zacferris@gmail.com>
2018-11-12 12:55:53 -0600

    Button fix
    fixed customer view req. assist btn.
    Moved around some Socket Code

--- public/javascripts/guestAssistanceHeader.js
+++ public/javascripts/guestAssistanceHeader.js
@@ -1,28 +1,18 @@
-//we are consolidating the content into this file. Rather than multiple event listeners for this one div, we'll do plenty.
-
-var theParent = document.querySelector("#requester"); //this creates a listener for this class.
-theParent.addEventListener("click", validateSelection, false); //no idea what false does. don't really care either.
 
 var socket = io();
+// socket.on('clearServiceRequest', function(){
+//     alert(tblNumber);
+// });
 
-socket.on('clearServiceRequest', function(id){
-    alert(id);
+
+$(document).ready(function(){
+    $("#servicerq").click(function(){
+        tblNumberString = tblNumber.toString();
+        socket.emit("serviceRequest", tblNumberString);  
+        alert("Request Sent")
+    });
+
+    $("#drinkrq").click(function(){
+        alert("Hello ");
+    });
 });
-
-
-function validateSelection(e) //e is the entity which was pressed.
-{
-    if (e.target !== e.currentTarget) {
-        var interactedItem = e.target.id; //saves the ID of the element clicked
-
-        if (interactedItem === "servicerq") {
-            // emit a service request event
-            tblNumberString = tblNumber.toString();
-            socket.emit("serviceRequest", tblNumberString);          
-        }
-        else if (interactedItem === "drinkrq") {
-            alert("Hello " + interactedItem);
-        }
-    }
-    e.stopPropagation();
-}
