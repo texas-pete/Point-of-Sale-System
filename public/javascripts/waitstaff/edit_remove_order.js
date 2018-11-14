4fd89b240336390b1b9bcf13cdbf42e0bc8e7510
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2018-11-14 11:12:58 -0600

    edit button and delete button function
    if two entries have the same name and description, they will probably both get deleted.

--- public/javascripts/waitstaff/edit_remove_order.js
+++ public/javascripts/waitstaff/edit_remove_order.js
@@ -6,6 +6,14 @@ function edit_remove(whichOperation) {
         let content = $("#item_name" + item).text();
         let desc = $("#item_desc" + item).text();//converted name.
         //we need to convert from a name to a string version of the object ID
+
+        var sidebarTblNum = $("#tblNumber").text(); //we need to clean up our input. No random garbage
+        for (let index = 0; index < 16; index++) {
+            if (sidebarTblNum == index) {
+                sidebarTblNum = index;
+            }
+        }
+
         $.ajax({
             url: "/fromNameToID/" + content,
             type: "POST",
@@ -13,16 +21,28 @@ function edit_remove(whichOperation) {
                 console.log(responseData);
 
                 $.ajax({
-                    url: "/removeSubmitted/" + responseData + "/" + desc, //I realized the fields inside of an array of objects
-                    type: "POST", //we probably need to send in the table number to actually retrieve the data. 
+                    url: "/removeSubmitted/" + responseData + "/" + desc + "/" + sidebarTblNum, //I realized the fields inside of an array of objects
+                    type: "POST",
                     sucess: function (responseData) {
-                        console.log("yeah");
+                        //we need to refresh the page
                     },
-                    error: console.error
+                    error: function (responseData) {
+                        //we need to refresh the page
+                    }
                 });
 
             },
-            error: console.error
+            error: function (responseData) { //if we have a fake item in our dbase, we can still remove it
+                $.ajax({
+                    url: "/removeSubmitted/" + content + "/" + desc + "/" + sidebarTblNum, //I realized the fields inside of an array of objects
+                    type: "POST",
+                    sucess: function (responseData) {
+                        //we need to resfresh the page
+                    },
+                    error: function (responseData) {
+                    }
+                });
+            }
         });
 
 
@@ -30,17 +50,47 @@ function edit_remove(whichOperation) {
     }
     else if (whichOperation.length == 5) {
         //we need to get the content within the #item_desc(num) content
-        let content = $("#item_desc" + item).text();
-        console.log(content);
+        let content = $("#item_name" + item).text();
+        let desc = $("#item_desc" + item).text();//converted name.
+        //we need to convert from a name to a string version of the object ID
 
-        // $.ajax({
-        //     url: "/editSubmitted/" + "test",
-        //     type: "POST",
-        //     sucess: function (responseData) {
-        //         console.log("yeah");
-        //     },
-        //     error: console.error
-        // });
+        var sidebarTblNum = $("#tblNumber").text(); //we need to clean up our input. No random garbage
+        for (let index = 0; index < 16; index++) {
+            if (sidebarTblNum == index) {
+                sidebarTblNum = index;
+            }
+        }
+        let newDescription = "ttest";
+        $.ajax({
+            url: "/fromNameToID/" + content,
+            type: "POST",
+            success: function (responseData) {
+                console.log(responseData);
+
+                $.ajax({
+                    url: "/editSubmitted/" + responseData + "/" + desc + "/" + sidebarTblNum + "/" + newDescription, //I realized the fields inside of an array of objects
+                    type: "POST",
+                    sucess: function (responseData) {
+                        //we need to refresh the page
+                    },
+                    error: function (responseData) {
+                        //we need to refresh the page
+                    }
+                });
+
+            },
+            error: function (responseData) { //if we have a fake item in our dbase, we can still remove it
+                $.ajax({
+                    url: "/editSubmitted/" + content + "/" + desc + "/" + sidebarTblNum + "/" + newDescription, //I realized the fields inside of an array of objects
+                    type: "POST",
+                    sucess: function (responseData) {
+                        //we need to resfresh the page
+                    },
+                    error: function (responseData) {
+                    }
+                });
+            }
+        });
 
 
 
