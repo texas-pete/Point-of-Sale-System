eba34c2d2da84f70d56a95fe5381e2105dc5df63
zacferris <36178696+zacferris@users.noreply.github.com>
2018-11-14 20:18:30 -0600

    header buttons work on all guest pages

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,3 +1,39 @@
+const socket = io();
+
+socket.on('connect', function() {
+    // Connected, let's sign-up for to receive messages for this room
+
+    //service requests
+    socket.emit("joinSR");
+    //drink requests
+    socket.emit("joinDR");
+ });
+
+// header button functions
+$(document).ready(function(){
+
+    // request assistance btn
+    $("#servicerq").click(function(){
+        tblNumberString = tblNumber.toString();
+        socket.emit("serviceRequest", tblNumberString);  
+        alert("Request Sent for Table: " + tblNumberString)
+    });
+
+
+    $(".drink").click(function(){
+        let drink = $(this).text().trim();
+        let tblNumberString = tblNumber.toString().trim();
+        alert(drink + " for table " + tblNumberString );
+        let requestMsg = {tableNum: tblNumberString, drink: drink};
+        socket.emit("drinkRequest", requestMsg);
+    });
+
+    socket.on("updatedSR", function(){
+        alert("UPDATED SR");
+    })
+});
+
+
 function showDrinks() {
 	document.getElementById("ass-drop").classList.toggle("show");
 }
