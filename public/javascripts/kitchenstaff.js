3c0dbe4e139b995bded193c2bc4f87873e088068
zacferris <zacferris@gmail.com>
2018-11-19 15:13:42 -0600

    show/hide cooking indicator

--- public/javascripts/kitchenstaff.js
+++ public/javascripts/kitchenstaff.js
@@ -1,33 +1,30 @@
-var currentTable;
+var currentID = 0;
 
-
-
-
-
-$(".order_display_sidebar").click(function () {
-    let id = $(this).attr("id");
-    divID = "_" + id;
-    divID2 = "__" + id;
-    console.log(divID);
-
-    // hide the others
-    $(".order_display_main").hide();
-    // show this one
-    $("#"+divID).show();
-
-    $("#Cooking").click(function () {
-
-       //if Cooking is clicked, turn status indicator on for the currently selected order 
-        $(divID2).show();
-    
-    });
-
-
-
-
-    //for removing order item
+//for removing order item
 $(document).ready(function () {
-	$('.Ready').on('click', removeFromOrder);
+	$('#Ready').on('click', removeFromOrder);
+
+	$("#Cooking").click(function () {
+
+		//if Cooking is clicked, turn status indicator on for the currently selected order 
+		if(currentID !== 0 ){
+			$('#__'+currentID).toggle();
+		}
+	 });
+
+	$(".order_display_sidebar").click(function () {
+		let id = $(this).attr("id");
+		currentID = id;
+		divID = "_" + id;
+		divID2 = "__" + id;
+		console.log(divID);
+
+		// hide the others
+		$(".order_display_main").hide();
+		// show this one
+		$("#"+divID).show();
+
+	});
 });
 
 //asks the user whether they're sure if they want to delete this item
@@ -48,8 +45,6 @@ function removeFromOrder(){
 		});
 	}
 }
-
-});
 
 
 
