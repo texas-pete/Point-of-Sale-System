1497dd4ff458a75657f05205e15430879dbc559a
Sledererer <36487110+Sledererer@users.noreply.github.com>
2018-11-20 19:10:47 -0600

    single pay

--- public/javascripts/pay.js
+++ public/javascripts/pay.js
@@ -65,11 +65,32 @@ document.getElementById("calculate").onclick = function() {
 
 
 $("#singleSub").click(function () {
+	var total = document.getElementById("overallTotal").innerHTML;
+	var tax = 	document.getElementById("totTax").innerHTML;
+	var tips = document.getElementById("totalTip").innerHTML;
+	console.log(total)
+	console.log(tax)
+	console.log(tips)
 
-	console.log("hello");
 
 
-}
+	$.ajax({ //there is a way to do this via $.post("/getTableOrder/" + sidebarVal) but keeping ajax as such
+		url: "/guest-pay/submit/" + tax + '/' + total + '/' + tips+ '/',
+		type: "POST",
+		success: function (retVal) { //responseData contains the returned 'name' from our ID
+
+		console.log("PS");
+
+		//we know we are only returning a string for a name.
+		//anytime we insert a value, we have to increment the q value because there are only q slots avaialble to store names.
+		},
+		error: function () { //if we can't find the name in our database
+			console.log("FAIL");
+		//anytime we insert a value, we have to increment the q value because there are only q slots avaialble to store names.
+		}
+	});
+
+});
 
 
 
